6a412ba68409199f159c7a37213fce4f1da3414b
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-11-01 11:56:36 +0100

    Personal DTN settings implemented;

--- apps/dtn/lib/AppInfo/Application.php
+++ apps/dtn/lib/AppInfo/Application.php
@@ -7,7 +7,7 @@
 namespace OCA\DTN\AppInfo;
 
 use OCP\AppFramework\App;
-use OCA\DTN\controller\DTNController;
+use OCA\DTN\controller\DtnController;
 use OCA\DTN\controller\ConfigProviderController;
 use OCP\IContainer;
 
@@ -23,8 +23,8 @@ class Application extends App {
 
         $container = $this->getContainer();
         $server = $container->getServer();
-        $container->registerService('DTNController', function (IContainer $container) use ($server) {
-            return new DTNController(
+        $container->registerService('DtnController', function (IContainer $container) use ($server) {
+            return new DtnController(
                     $container->query('AppName'), $server->getRequest(), $server->getUserSession(), $server->getConfig(), $container->query('Logger')
             );
         });
@@ -34,8 +34,16 @@ class Application extends App {
                     $container->query('AppName'), $server->getRequest(), $server->getUserManager(), $server->getUserSession(), $server->getConfig(), $container->query('Logger')
             );
         });
-        
-//        $server->getConfig()->setUserValue('admin', 'dtn', 'dtnID', 'test01@dtn.nl');
+
+        $container->registerService('DtnSettingsController', function (IContainer $container) use ($server) {
+            return new ConfigProviderController(
+                    $container->query('AppName'), $server->getRequest(), $server->getUserManager(), $server->getUserSession(), $server->getConfig(), $container->query('Logger')
+            );
+        });
+
+//        $server->getConfig()->deleteAppValue('dtn', 'dtnUID');
+//        $server->getConfig()->setUserValue('admin', 'dtn', 'dtnUID', 'admin@dtn-agent.com');
+//        $server->getConfig()->setUserValue('antoon', 'dtn', 'dtnUID', 'antoonp@dtn-agent.com');
 //        $server->getConfig()->setUserValue('antoon', 'dtn', 'dtnID', 'antoon@dtn.nl');
 //        $server->getConfig()->setUserValue('pietje', 'dtn', 'dtnID', 'pietje@dtn.nl');
     }
