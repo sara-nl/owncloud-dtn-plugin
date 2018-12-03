cb8029b6a98e918e70bae318fde0cc239f4cd990
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-03 11:43:36 +0100

    Docker container run/stop added. Exit code added.

--- resources/jenkins/all-unit-tests.bat
+++ resources/jenkins/all-unit-tests.bat
@@ -1,1 +1,13 @@
-cd c:\xampp\htdocs\ownCloud-DTN-plugin-docker & docker exec owncloud-dtn-plugin /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+SET /A errno=0
+
+cd c:\xampp\htdocs\ownCloud-DTN-plugin-docker
+docker-compose up -d & docker exec owncloud-dtn-plugin /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+IF /I "%ERRORLEVEL%" NEQ "0" ( 
+    SET /A errno=%ERRORLEVEL%
+)
+docker-compose stop
+IF /I "%ERRORLEVEL%" NEQ "0" ( 
+    SET /A errno=%ERRORLEVEL%
+)
+Exit /B %errno%
