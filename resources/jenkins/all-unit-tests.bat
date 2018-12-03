4ac95d628271769e22e317a2534f44ed8fd68b0e
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-03 13:11:41 +0100

    new container name.

--- resources/jenkins/all-unit-tests.bat
+++ resources/jenkins/all-unit-tests.bat
@@ -2,9 +2,9 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET /A errno=0
 
 cd resources/docker
-:: cd c:\xampp\htdocs\ownCloud-DTN-plugin-docker
+::cd c:\xampp\htdocs\ownCloud-DTN-plugin\resources\docker
 docker-compose up -d
-docker-compose exec owncloud /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+docker exec owncloud-dtn-plugin-unit-tests-runner /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
 IF /I "%ERRORLEVEL%" NEQ "0" ( 
     SET /A errno=%ERRORLEVEL%
 )
