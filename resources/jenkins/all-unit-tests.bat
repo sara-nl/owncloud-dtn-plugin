711fb79afebd0379d10e0b5d35abd7bb1c9487eb
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-03 12:41:05 +0100

    path fixed.

--- resources/jenkins/all-unit-tests.bat
+++ resources/jenkins/all-unit-tests.bat
@@ -1,7 +1,7 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET /A errno=0
 
-cd ..\docker
+cd resources/docker
 :: cd c:\xampp\htdocs\ownCloud-DTN-plugin-docker
 docker-compose up -d
 docker-compose exec owncloud /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
