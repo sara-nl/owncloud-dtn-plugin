eab70127fff4ce8b0f8c77bae644477f2055fa31
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-03 15:51:36 +0100

    Licence notification added.

--- apps/dtn/lib/Controller/DtnSettingsController.php
+++ apps/dtn/lib/Controller/DtnSettingsController.php
@@ -1,9 +1,19 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2018 SURFsara (http://www.surfsara.nl)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace OCA\DTN\Controller;
@@ -50,18 +60,19 @@ class DtnSettingsController extends Controller {
      * @param type $value
      */
     public function setUserSetting($key, $value) {
-        if(isset($key) && isset($value)) {
+        if (isset($key) && isset($value)) {
             $this->config->setUserValue($this->userSession->getUser()->getUID(), 'dtn', $key, trim($value));
         }
     }
-    
+
     /**
      * @param type $key
      * @param type $value
      */
     public function setAdminSetting($key, $value) {
-        if(isset($key) && isset($value)) {
+        if (isset($key) && isset($value)) {
             $this->config->setAppValue('dtn', $key, trim($value));
         }
     }
+
 }
--- apps/dtn/tests/unit/lib/UtilTest.php
+++ apps/dtn/tests/unit/lib/UtilTest.php
@@ -1,7 +1,19 @@
 <?php
 
-/*
- * SURFsara
+/**
+ * Copyright 2018 SURFsara (http://www.surfsara.nl)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace OCA\DTN\Tests;
@@ -38,23 +50,6 @@ class UtilTest extends \Test\TestCase {
     public function testFindUserForDTNUserId() {
         /* This user does not exist */
         $this->assertNull(\OCA\DTN\Util::findUserForDTNUserId('xyz'));
-        
-        /* Setup users */
-//        $_user = $this->createMock('\OCP\IUser');
-//        $_user->expects($this->any())
-//                ->method('getUID')
-//                ->will($this->returnValue('someUserId'));
-//        $_users = [];
-//        $_users[0] = $_user;
-//        $this->config->expects($this->any())
-//                ->method('getUsersForUserValue')
-//                ->will($this->returnValue($_users));
-//        $this->server->expects($this->any())
-//                ->method('getConfig')
-//                ->will($this->returnValue($this->config));
-//        
-//        
-//        $this->assertNotNull(\OCA\DTN\Util::findUserForDTNUserId('someUserId'));
     }
 
 }
