316d7c37a3cd8557cce1421115809e2b733ce7cf
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-03 11:54:46 +0100

    minor fix.

--- resources/jenkins/all-unit-tests.bat
+++ resources/jenkins/all-unit-tests.bat
@@ -1,7 +1,6 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET /A errno=0
 
-#cd c:\xampp\htdocs\ownCloud-DTN-plugin-docker
 cd resources\docker
 docker-compose up -d & docker exec owncloud-dtn-plugin /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
 IF /I "%ERRORLEVEL%" NEQ "0" ( 
