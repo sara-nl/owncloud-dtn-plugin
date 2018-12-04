809b1f70e411d5146620aecb5ed3fdc07950b1ef
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-04 15:56:32 +0100

    sudo removed

--- resources/jenkins/all-unit-tests.sh
+++ resources/jenkins/all-unit-tests.sh
@@ -4,13 +4,13 @@
 errno=0
 cd resources/docker
 
-sudo docker-compose up -d
-sudo docker exec owncloud-dtn-plugin-unit-tests-runner /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+docker-compose up -d
+docker exec owncloud-dtn-plugin-unit-tests-runner /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
 ## save the test exit result in case the tests fail
 
-sudo docker-compose stop
+docker-compose stop
 
 ## clean up containers and their volumes
-sudo docker rm owncloud-dtn-plugin-unit-tests-runner -v -f
-sudo docker rm owncloud-dtn-plugin-unit-tests-runner-db -v -f
-sudo docker rm owncloud-dtn-plugin-unit-tests-runner-redis -v -f
+docker rm owncloud-dtn-plugin-unit-tests-runner -v -f
+docker rm owncloud-dtn-plugin-unit-tests-runner-db -v -f
+docker rm owncloud-dtn-plugin-unit-tests-runner-redis -v -f
