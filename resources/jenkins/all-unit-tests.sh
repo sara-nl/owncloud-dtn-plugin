570b39d38da0c659bc0bd1bf9d9f16488c19ea36
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-04 13:52:17 +0100

    Modify the test runner script

--- resources/jenkins/all-unit-tests.sh
+++ resources/jenkins/all-unit-tests.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-# UNIX test runner script
-msg1=Hello
-msg2=There!
-echo $msg1 $msg2
+## UNIX test runner script
+
+errno=0
+cd resources/docker
+
+docker-compose up -d
+docker exec owncloud-dtn-plugin-unit-tests-runner /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+## save the test exit result in case the tests fail
+
+docker-compose stop
+
+## clean up containers and their volumes
+docker rm owncloud-dtn-plugin-unit-tests-runner -v -f
+docker rm owncloud-dtn-plugin-unit-tests-runner-db -v -f
+docker rm owncloud-dtn-plugin-unit-tests-runner-redis -v -f
