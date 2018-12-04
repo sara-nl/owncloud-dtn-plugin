7056da8725e80dcebe181d93ae6b3738f7ad22a7
antoonp <antoonp@W0417010.ka.surfsara.nl>
2018-12-04 09:35:23 +0100

    Comments added.

--- resources/jenkins/all-unit-tests.bat
+++ resources/jenkins/all-unit-tests.bat
@@ -1,18 +1,25 @@
+:: Windows test runner script
+
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 SET /A errno=0
 
+:: switch to location of docker-compose file
 cd resources/docker
-::cd c:\xampp\htdocs\ownCloud-DTN-plugin\resources\docker
+
 docker-compose up -d
 docker exec owncloud-dtn-plugin-unit-tests-runner /bin/bash -c "cd /var/www/owncloud/apps/dtn/tests/unit && php phpunit --colors --debug --bootstrap bootstrap.php --filter 'OCA\\DTN\\Tests' ./"
+:: save the test exit result in case the tests fail
 IF /I "%ERRORLEVEL%" NEQ "0" ( 
     SET /A errno=%ERRORLEVEL%
 )
 docker-compose stop
+
+:: clean up containers and their volumes
 docker rm owncloud-dtn-plugin-unit-tests-runner -v -f
 docker rm owncloud-dtn-plugin-unit-tests-runner-db -v -f
 docker rm owncloud-dtn-plugin-unit-tests-runner-redis -v -f
 IF /I "%ERRORLEVEL%" NEQ "0" ( 
     SET /A errno=%ERRORLEVEL%
 )
+:: exit with the test result
 Exit /B %errno%
